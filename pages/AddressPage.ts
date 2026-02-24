b134789b1bf846204e272de36c1419370fc42ccd
Shakhawat1998 <shopicuet17@gmail.com>
2026-02-25 02:03:39 +0600

    test: enhance address page tests and page object
    - Added methods to AddressPage for filling and saving address forms.
    - Updated 4AddressPage.spec.ts to automate address creation using random email generation.

--- pages/AddressPage.ts
+++ pages/AddressPage.ts
@@ -5,12 +5,28 @@ export class AddressPage extends BasePage {
 
   private myAccountAddressHeading: Locator;
   private addNewAddressButton: Locator;
+  private firstNameInput: Locator;
+  private lastNameInput: Locator;
+  private countryDropdown: Locator;
+  private cityInput: Locator;
+  private address1Input: Locator;
+  private emailInput: Locator;
+  private phoneNumberInput: Locator;
+  private saveButton: Locator;
 
   constructor(page: Page) {
     super(page);
 
     this.myAccountAddressHeading = page.locator("//h1[normalize-space()='My account - Addresses']");
     this.addNewAddressButton = page.locator("//button[normalize-space()='Add new']");
+    this.firstNameInput = page.locator("#Address_FirstName");
+    this.lastNameInput = page.locator("#Address_LastName");
+    this.emailInput = page.locator("#Address_Email");
+    this.countryDropdown = page.locator("#Address_CountryId");
+    this.cityInput = page.locator("#Address_City");
+    this.address1Input = page.locator("#Address_Address1");
+    this.phoneNumberInput = page.locator("#Address_PhoneNumber");
+    this.saveButton = page.locator("//button[normalize-space()='Save']");
   }
 
   getMyAccountAddressHeading(): Locator {
@@ -21,6 +37,19 @@ export class AddressPage extends BasePage {
     await this.addNewAddressButton.click();
   }
 
-  
+  async fillAddressForm(firstName: string, lastName: string, email: string, countryOption: string, city: string, address1: string, phoneNumber: string) {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.emailInput.fill(email);
+    await this.countryDropdown.selectOption({value: countryOption});
+    await this.cityInput.fill(city);
+    await this.address1Input.fill(address1);
+    await this.phoneNumberInput.fill(phoneNumber);
+  }
 
+  async clickSaveButton() {
+    await this.saveButton.click();
+  }
+
+  
 }
--- tests/4AddressPage.spec.ts
+++ tests/4AddressPage.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "./fixtures/baseTest";
 import { HomePage } from "../pages/HomePage";
 import { MyAccountPage } from "../pages/MyAccountPage";
 import { AddressPage } from "../pages/AddressPage";
+import { RandomUtils } from "../utils/randomUtils";
 
 
 test("Verify user is redirected to My Account page", async ({ page }) => {
@@ -27,6 +28,12 @@ test("Verify user is redirected to address page", async ({ page }) => {
 
 test("Verify that new address is added by completing address form", async ({ page }) => {
   const addressPage = new AddressPage(page);
+  const email = RandomUtils.getRandomEmail("user", "gmail.com");
 
   await addressPage.clickAddNewAddressButton();
+  await addressPage.fillAddressForm("Shakhawat", "Hossain", email, "236", "New York", "123 Main Street", "1234567890");
+  await addressPage.clickSaveButton();
+
 });
+
+
